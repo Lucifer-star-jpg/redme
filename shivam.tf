9ff5e7661a6df5570a23a54187b4439fe90d11cf
Lucifer-star-jpg <shivamkate911@gmail.com>
2025-03-31 16:39:35 +0530

    added pub sub

--- shivam.tf
+++ shivam.tf
@@ -6,3 +6,12 @@ resource "aws_vpc" "main" {
   cidr_block = "10.10.0.0/16"
   tags = { Name = "JenkinsVPC" }
 }
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.10.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = { Name = "JenkinsSubnet" }
+}
